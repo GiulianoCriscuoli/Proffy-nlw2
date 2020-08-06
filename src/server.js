11b75adee8d2aaf6625eb6b329d572a0a19eb4ea
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-08-06 16:35:49 -0300

    Dinamizando o envio de objetos com o nunjucks

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const proffys = [
         name: "Diego Fernandes", 
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
         whatsapp: 985746372, 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "química", 
         cost: "20", 
         weekday: [0
@@ -16,7 +16,7 @@ const proffys = [
         name: "Diega Fernandes", 
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
         whatsapp: 985746372, 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "química", 
         cost: "20", 
         weekday: [0
@@ -27,27 +27,25 @@ const proffys = [
         }
 ]
 
-
-
 /* html da home */
 
 const landingPage = (req, res) => {
 
-    res.sendFile(__dirname + "/views/index.html");
+    res.render("index.html");
 }
 
 /* html do study */
 
 const study = (req, res) => {
 
-    res.sendFile(__dirname + "/views/study.html");
+    res.render("study.html", {proffys});
 }
 
 /* html do give-classes */
 
 const giveClasses = (req, res) => {
 
-    res.sendFile(__dirname + "/views/give-classes.html");
+    res.render("give-classes.html");
 }
 
 const express = require('express');
@@ -74,6 +72,6 @@ app.get('/', landingPage );
 app.get('/study', study);
 app.get('/give-classes', giveClasses);
 
-const port = app.listen(5500);
+const PORT = app.listen(5500);
 
-console.log("Está rodando na porta: ",port);
+console.log("Está rodando na porta: ", PORT.address().port);
